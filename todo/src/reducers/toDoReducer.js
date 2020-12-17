3cc933507c3713e4186672551658af03cbd6ac8e
Clare Jackson <clare-jackson@lambdastudents.com>
2020-12-16 22:35:50 -0500

    Started the reducer for state.

---
+++ todo/src/reducers/toDoReducer.js
@@ -0,0 +1,14 @@
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
+};
+
+export const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+];
+
